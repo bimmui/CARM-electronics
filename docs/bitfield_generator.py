75e47f544163599b0770c88d6c63a92f68fc1b1e
bimmui <58013370+bimmui@users.noreply.github.com>
2024-01-14 23:51:54 -0500

    finalized bitfield schema

--- docs/bitfield_generator.py
+++ docs/bitfield_generator.py
@@ -1,4 +1,7 @@
-"""This module provides generates the bitfield schema seen in docs\\diagrams\\bitfield_diagram.png.
+# Author(s):
+#   Daniel Opara <hello@danielopara.me>
+
+"""This script generates the bitfield schema seen in docs\\diagrams\\bitfield_diagram.png.
 It utilizes the Python port of the bitfield module developed by Arth-ur.
 
 For information on how to produce a similar diagram, visit https://github.com/Arth-ur/bitfield for the Python port
@@ -12,19 +15,36 @@ from bit_field import render, jsonml_stringify
 
 # Register description in JSON format
 bit_organization = [
-    {"name": "Frag #", "bits": 2, "type": 4},
-    {"name": "Time (s)", "bits": 9, "type": 5},
-    {"name": "Internal Temp Sensor", "bits": 14, "type": 6},
-    {"name": "External Temp Sensor", "bits": 7, "type": 6},
-    {"name": "Barometric Pressure", "bits": 7, "type": 7},
+    {"name": "Frag Num", "bits": 3, "type": 4},
+    {"name": "GPS LAT", "bits": 28, "type": 5},
+    {"name": "ND", "bits": 1},
     ################################################################
-    {"name": "Frag #", "bits": 2, "type": 4},
-    {"name": "Altitude", "bits": 12, "type": 5},
-    {"name": "Acceleration", "bits": 7, "type": 6},
-    {"name": "Gyro measurements", "bits": 7, "type": 7},
+    {"name": "Frag Num", "bits": 3, "type": 4},
+    {"name": "GPS LONG", "bits": 29, "type": 5},
+    ################################################################
+    {"name": "Frag Num", "bits": 3, "type": 4},
+    {"name": "ALT", "bits": 16, "type": 7},
+    {"name": "EXT TEMP", "bits": 13, "type": 2},
+    ################################################################
+    {"name": "Frag Num", "bits": 3, "type": 4},
+    {"name": "ACCEL", "bits": 16, "type": 8},
+    {"name": "INT TEMP AVBAY", "bits": 12, "type": 2},
+    {"name": "ND", "bits": 1},
+    ################################################################
+    {"name": "Frag Num", "bits": 3, "type": 4},
+    {"name": "BAR PR", "bits": 16, "type": 6},
+    {"name": "INT TEMP ENGINE", "bits": 12, "type": 2},
+    {"name": "ND", "bits": 1},
+    ################################################################
+    {"name": "Frag Num", "bits": 3, "type": 4},
+    {"name": "GYRO", "bits": 14, "type": 10},
+    {"name": "TIME", "bits": 12, "type": 9},
+    {"name": "ND", "bits": 3},
+    ################################################################
 ]
 
-options = {"hspace": 888, "lanes": 3, "bits": 64}
+options = {"hspace": 888, "lanes": 6, "bits": 192, "vflip": True, "hflip": True}
+
 
 # Render the bit field diagram
 jsonml = render(bit_organization, **options)
