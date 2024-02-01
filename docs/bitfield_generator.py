b80a9c816b3585fca4ca788cb1b9b5b6d9a447ee
Daniel Opara <58013370+bimmui@users.noreply.github.com>
2024-01-31 23:37:17 -0500

    resolved "overflow" bugs in bitfield_organizer and made the generator completely dependent on json, also FINISHED TWO SCHEMAS YAHHH

--- docs/bitfield_generator.py
+++ docs/bitfield_generator.py
@@ -16,25 +16,29 @@ from bit_field import render, jsonml_stringify
 
 # note: typeColor() in bit_field was modified to add more colors. To acheive the same affect,
 # edit typeColor(t) and change the "styles" dict to the following:
-"""styles = {
-        '2': 0,
-        '3': 80,
-        '4': 170,
-        '5': 45,
-        '6': 126,
-        '7': 215,
-        '8': 20,
-        '9': 420,
-        '10': 200,
-        '11': 250
-    }"""
+"""
+styles = {
+    '2': 0,
+    '3': 80,
+    '4': 170,
+    '5': 45,
+    '6': 126,
+    '7': 215,
+    '8': 20,
+    '9': 275,
+    '10': 200,
+    '11': 250,
+    '12': 1000
+}
+"""
 
 
 def read_schema_json():
     filename = input("Enter the name of the JSON file (without extension): ")
     data = None
+    path = f"bitfield-schemas\\{filename}.json"
     try:
-        with open(filename + ".json", "r") as file:
+        with open(path, "r") as file:
             lines = file.readlines()
             # Ignore lines starting with #
             filtered_lines = [line for line in lines if not line.startswith("#")]
@@ -46,21 +50,20 @@ def read_schema_json():
         print(f"File '{filename}.json' not found.")
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e}")
-    return data
+    return data[0], data[1], filename
 
 
 # Register description in JSON format
-bit_organization = read_schema_json()
+# Tweak the options in the json file based on the schema you are generating
+bit_organization, options, schema_name = read_schema_json()
 
+# Convert the list of dictionaries to a single dictionary
+# i used gpt for this shit i couldnt bro
 options = {
-    "vspace": 100,
-    "hspace": 1400,
-    "lanes": 8,
-    "bits": 256,
-    "hflip": True,
-    "fontsize": 13,
+    key: entry[key] if not isinstance(entry[key], bool) else str(entry[key]).lower()
+    for entry in options
+    for key in entry
 }
-
 
 # Render the bit field diagram
 jsonml = render(bit_organization, **options)
@@ -73,5 +76,5 @@ png_data = cairosvg.svg2png(bytestring=html)
 png_image = Image.open(BytesIO(png_data))
 
 # Save the PNG file
-png_file_path = "diagrams\\bitfield_schema.png"
+png_file_path = f"diagrams\\{schema_name}.png"
 png_image.save(png_file_path, "PNG")
