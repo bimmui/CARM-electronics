9d6276a7288e2a7c17612df795c181c04d7bfb01
Daniel Opara <58013370+bimmui@users.noreply.github.com>
2024-01-31 23:37:17 -0500

    resolved "overflow" bugs in bitfield_organizer and made the generator completely dependent on json, also FINISHED TWO SCHEMAS YAHHH

--- docs/bitfield_organizer.py
+++ docs/bitfield_organizer.py
@@ -1,19 +1,36 @@
 # Author(s):
 #   Daniel Opara <hello@danielopara.me>
 
+from copy import deepcopy
+
 """This script prints out how to organize your bitwords given the names and 
 length of your bitfields. This was used to determine the bitfield schema generated in 
 bitfield_generator.py.
 """
 
 # Change these values to reflect your project!
-HEADER_FIELD = 4
+HEADER_FIELD = 8
 WORD_SIZE = 32
 
 
-def distribute_bitfields(bitfields, max_value_per_bucket):
+def distribute_bitfields(bitfields, max_value_per_bucket, swap_indices=None):
     # sort bit field values in descending order
     sorted_bitfields = sorted(bitfields, key=lambda x: x[1], reverse=True)
+    before_swaps = deepcopy(sorted_bitfields)
+
+    # sometimes descending order isnt the best way to iterating through the array
+    # and organize bit, we can swap indicies if we want to add some field in a
+    # bucket in a particular order
+    if swap_indices:
+        for swap_index1, swap_index2 in swap_indices:
+            sorted_bitfields[swap_index1], sorted_bitfields[swap_index2] = (
+                sorted_bitfields[swap_index2],
+                sorted_bitfields[swap_index1],
+            )
+
+    print("Before Sorting\t\t\t\t\tAfter Sorting")
+    for before, after in zip(before_swaps, sorted_bitfields):
+        print(f"{before}\t\t\t\t\t{after}")
 
     # initialize a single word with the value of the header field
     buckets = [[HEADER_FIELD]]
@@ -34,41 +51,64 @@ def distribute_bitfields(bitfields, max_value_per_bucket):
         # if the field can't fit in existing buckets, distribute it across multiple buckets
         if not field_added:
             remaining_value = field_value
-            while remaining_value > 0:
-                # create a new bucket for the remaining part of the field
-                new_bucket = [HEADER_FIELD]
+            if sum(buckets[-1]) == max_value_per_bucket:
+                new_bucket = [HEADER_FIELD, remaining_value]
                 new_bucket_name = ["HEADER_FIELD", field_name]
-
-                # determine how much of the field can fit in the new bucket
-                amount_to_add = min(max_value_per_bucket - sum(new_bucket), remaining_value)
-
-                # add the partial field to the new bucket
-                new_bucket.append(amount_to_add)
-                bucket_names[-1].append(field_name)
-
-                remaining_value -= amount_to_add
-
                 buckets.append(new_bucket)
                 bucket_names.append(new_bucket_name)
+            else:
+                while remaining_value > 0:
+                    # create a new bucket for the remaining part of the field
+                    new_bucket = [HEADER_FIELD]
+                    new_bucket_name = ["HEADER_FIELD", field_name]
+
+                    # check if there's some space in the prev bucket
+                    # if there is, put it there
+                    if sum(buckets[-1]) < max_value_per_bucket:
+                        # determine how much of the field can fit in the prev bucket
+                        partial_prev = max_value_per_bucket - sum(buckets[-1])
+                        partial_new = remaining_value - partial_prev
+
+                        # add the partial fields to the prev bucket and new buckets
+                        new_bucket.append(partial_new)
+                        buckets[-1].append(partial_prev)
+                        bucket_names[-1].append(field_name)
+
+                        remaining_value -= partial_prev + partial_new
+
+                        buckets.append(new_bucket)
+                        bucket_names.append(new_bucket_name)
 
     return buckets, bucket_names
 
 
 # list of bitfields and their values, replace with your bitfields as needed
 bitfield_list = [
-    ("time", 8),
+    ("gps coord: lat", 28),
+    ("num satellites", 3),
     ("external temp", 14),
     ("internal temp: avbay", 12),
     ("internal temp: engine bay", 12),
+    ("velocity", 9),
     ("altitude", 17),
-    ("barometric pressure", 15),
-    ("acceleration", 18),
-    ("gyro shit", 10),
-    ("gps coord: latitude", 28),
-    ("gps coord: longitude", 29),
+    ("accel x", 14),
+    ("accel y", 14),
+    ("accel z", 14),
+    ("gyro x", 23),
+    ("gyro y", 23),
+    ("gyro z", 16),
+    ("mag x", 11),
+    ("mag y", 11),
+    ("mag z", 11),
+    ("gps coord: long", 29),
+    ("failure flag", 13),
 ]
 
-bitfield_vals, bitfield_names = distribute_bitfields(bitfield_list, WORD_SIZE)
+swapper = [
+    (0, 1),
+]
+
+bitfield_vals, bitfield_names = distribute_bitfields(bitfield_list, WORD_SIZE, swapper)
 
 print()
 print(f"Arrange your {WORD_SIZE} bit words according to the buckets shown below:")
@@ -76,7 +116,7 @@ print("-------------------------------------------------------------------")
 # used gpt for this printing part
 for i, (vals, names) in enumerate(zip(bitfield_vals, bitfield_names)):
     # Print left-justified values and names
-    # was a little scuffed so had to subtract 55, should be good all around tho
+    # was a little scuffed so had to subtract 55, should be good all around tho, change the number as you please
     print(
-        f"Bucket {i + 1}: {vals}{' ' * (len(str(bitfield_vals)) - 55 - len(str(vals)))}  {names}"
+        f"Bucket {i + 1}: {vals}{' ' * (len(str(bitfield_vals)) - 120 - len(str(vals)))}  {names}"
     )
