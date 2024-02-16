803eae156b28a134e6ada891de24b13ba285c330
Daniel Opara <58013370+bimmui@users.noreply.github.com>
2024-02-16 00:15:03 -0500

    starting unit tests and state changer

--- carm-electronics/flight-computer/bb_setup.h
+++ carm-electronics/flight-computer/bb_setup.h
@@ -3,7 +3,7 @@
  *                     bb_setup.h
  *
  *     Author(s):  Daniel Opara,
- *     Date:       1/6/2023
+ *     Date:       1/6/2024
  *
  *     Overview: Contains functions for setting up configurations for the breakout
  *                  boards connected to the Feather M0, this includes the sensor breakout
