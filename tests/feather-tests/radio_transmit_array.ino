b8d09c153bd4d9572365a322047a34c29d43513b
bimmui <58013370+bimmui@users.noreply.github.com>
2024-03-24 00:51:16 -0400

    tested radio and made generic funcs for transmitting and receiving data

---
+++ tests/feather-tests/radio_transmit_array.ino
@@ -0,0 +1,88 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <inttypes.h>
+
+#if defined(ADAFRUIT_FEATHER_M0) || defined(ADAFRUIT_FEATHER_M0_EXPRESS) || defined(ARDUINO_SAMD_FEATHER_M0) // Feather M0 w/Radio
+#define RFM95_CS 8
+#define RFM95_INT 3
+#define RFM95_RST 4
+
+#endif
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 434.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+void setup()
+{
+    pinMode(RFM95_RST, OUTPUT);
+    digitalWrite(RFM95_RST, HIGH);
+
+    Serial.begin(115200);
+    while (!Serial)
+        delay(1);
+    delay(100);
+
+    Serial.println("Feather LoRa TX Test!");
+
+    // manual reset
+    digitalWrite(RFM95_RST, LOW);
+    delay(10);
+    digitalWrite(RFM95_RST, HIGH);
+    delay(10);
+
+    while (!rf95.init())
+    {
+        Serial.println("LoRa radio init failed");
+        Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+        while (1)
+            ;
+    }
+    Serial.println("LoRa radio init OK!");
+
+    // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+    if (!rf95.setFrequency(RF95_FREQ))
+    {
+        Serial.println("setFrequency failed");
+        while (1)
+            ;
+    }
+    Serial.print("Set Freq to: ");
+    Serial.println(RF95_FREQ);
+
+    // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+    // The default transmitter power is 13dBm, using PA_BOOST.
+    // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+    // you can set transmitter powers from 5 to 23 dBm:
+    rf95.setTxPower(23, false);
+}
+
+int16_t packetnum = 0; // packet counter, we increment per xmission
+
+void loop()
+{
+    delay(1000);                       // Wait 1 second between transmits, could also 'sleep' here!
+    Serial.println("Transmitting..."); // Send a message to rf95_server
+
+    uint64_t radiopacket[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    Serial.print("Sending ");
+    for (int i = 0; i < 20; i++)
+    {
+        Serial.print("radiopacket[");
+        Serial.print(i);
+        Serial.print("] = ");
+        Serial.println(radiopacket[i]);
+    }
+    Serial.println(sizeof(radiopacket));
+
+    Serial.println("Sending...");
+    delay(10);
+    rf95.send((uint8_t *)radiopacket, sizeof(radiopacket));
+
+    Serial.println("Waiting for packet to complete...");
+    delay(10);
+    rf95.waitPacketSent();
+}
